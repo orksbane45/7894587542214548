2a8b7820ec25a73d15863e4c5caaca7844d66bd5
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-05 19:06:39 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,10 +4,10 @@ exports.run = (client, message) => {
 
 
         if (msg.createdTimestamp - message.createdTimestamp < 200)
-      msg.edit('Pong! ( <:ping1:441979318575562763> : ' + (msg.createdTimestamp - message.createdTimestamp) + 'ms)');
+      msg.edit('Pong! ( <:IZBgoodping:453594990304821278> : ' + (msg.createdTimestamp - message.createdTimestamp) + 'ms)');
 
   if (msg.createdTimestamp - message.createdTimestamp >= 200)
-      msg.edit('Pong! ( <:ping3:441965765445419018> : ' + (msg.createdTimestamp - message.createdTimestamp) + 'ms)');
+      msg.edit('Pong! ( <:IZBbadping:453594938236469248> : ' + (msg.createdTimestamp - message.createdTimestamp) + 'ms)');
 
 
     });
