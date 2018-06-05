77e4bf92f60624b4fce672690d4014b68fa658e2
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-05 18:49:53 +0200

    Update master.js

--- commands/master.js
+++ commands/master.js
@@ -2,7 +2,6 @@
   exports.run = (client, message) => {
   
   if (message.author.id === settings.ownerid) return message.reply("Tu est mon maître !")
-  if (message.author.id === settings.happy) return message.reply("Je parle pas au pd !")
   if (message.author.id === settings.g8go) return message.reply("Tu est mon second maître !")
        message.channel.send("Tu n'est pas mon maître")                       
 };
