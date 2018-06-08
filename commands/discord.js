5fcb66d2f6d11cf3f6385b844dc69b7041b14c11
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-08 18:12:20 +0200

    Update discord.js

--- commands/discord.js
+++ commands/discord.js
@@ -5,6 +5,7 @@ exports.run = function(client, message, args) {
 
   .addField("Discord officiel", "<:IZBdiscord:453594581494398976> https://discord.gg/82DaHX8")
   .addField("La Meute","<:IZBlameute:453599845609898024> https://discord.gg/jMrtt6k")
+  .addField("L'iglou","<:IZBliglou:453637824365723666> https://discord.gg/Zk2ezKC")
   .setColor('RANDOM')
   .setTimestamp()
 
