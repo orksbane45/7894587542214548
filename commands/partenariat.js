5bb4ecd5ae8ba638b212a069c5757e84ca39a69e
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-10 20:52:46 +0200

    Add files via upload

---
+++ commands/partenariat.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+
+  const msg = new Discord.RichEmbed()
+
+    .setColor("RANDOM")
+    .setTitle("Liste des Discord partenaire :")
+    .addField(" La Meute", "<:IZBlameute:453599845609898024> https://discord.gg/jMrtt6k")
+    .addField(" L'iglou", "<:IZBliglou:453637824365723666> SOON")
+
+
+
+
+
+    message.channel.send(msg)
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['pt'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'partenariat',
+  description: 'null',
+  usage: 'partenariat'
+};
