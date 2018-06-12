fdd788adc288f70c9703962381548a50fb5b4563
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-12 19:05:57 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
     message.delete()
     var embed = new Discord.RichEmbed()
     .setColor("RANDOM")
-    .setDescription(`${message.author.username} á dit :` + args.join(" "))
+    .setDescription(`${message.author.username} :` + args.join(" "))
     message.channel.send(embed)
 
     }
