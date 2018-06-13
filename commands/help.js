7df14dfaa88e13bd45fbcb0fa265c60e2b42f642
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-13 13:20:14 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,10 @@
-const Discord = require('discord.js');
-exports.run = function(client, message, args) {
+const settings = require('../settings.json');
+const Discord = require("discord.js");
+
+exports.run = (client, message, params) => {
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
 
   const msg = new Discord.RichEmbed()
 
@@ -9,11 +14,26 @@ exports.run = function(client, message, args) {
   .addField('<:IZBgames:453594840374968350>  Fun', '`.roll`, `.master`, `.8ball`, `.hug`, `.kiss`, `.cat`, `.hack`')
   .addField('<:IZBinfo:453594898625593344> Infos',' `.stats`, `.invite`,`.site`, `.discord`, `.ping`, `.help`, `.profile`, `.google`, `.avatar`')
   .addField('<:IZBsupport:453595116297519125> Supports', '`.hsupport`, `.support`')
- 
+  .setDescription("Utilisez `.help <commandes> pour plus d'informations !`")
   .setTimestamp()
 
-              message.channel.send(msg)
-}
+              message.channel.send(msg);
+
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+
+      const msg2 = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(`**Commande :** ${command.help.name}`)
+      .setDescription(`Description : ${command.help.description}\nUsage : ${command.help.usage}`)
+  message.channel.send(msg2)
+    }
+      
+  }
+};
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
