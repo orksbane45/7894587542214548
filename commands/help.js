f99e719d691b43fa9c3d8789d74ccfb5c58cf2b6
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-24 16:50:09 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ exports.run = (client, message, params) => {
   .setColor('RANDOM')
   .setAuthor('Liste des commandes :')
   .addField(':rotating_light: Administrations', '`.reload`, `.purge`, `.warn`, `.mute`, `.say`, `.sayimg`, `.kick`, `.ban`,`.vote`')
-  .addField('<:IZBgames:459689915039612928> Fun', '`.roll`, `.master`, `.8ball`, `.hug`, `.kiss`, `.cat`, `.hack`')
+  .addField('<:IZBgames:459689915039612928> Fun', '`.roll`, `.master`, `.8ball`, `.hug`, `.kiss`, `.cat`, `.hack`, `.codesecret`')
   .addField('<:IZBinfo:459689946513408000> Infos',' `.stats`, `.invite`,`.site`, `.discord`, `.ping`, `.help`, `.profile`, `.google`, `.avatar`, `.partenariat`')
   .addField('<:IZBsupport:459690005246377995> Supports', '`.hsupport`, `.support`')
   .setDescription("Utilisez `.help <commandes> pour plus d'informations !`")
