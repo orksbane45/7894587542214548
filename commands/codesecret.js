c63278ad4d76eecf49ddc1ac78b22613b29e0c18
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-24 14:10:16 +0200

    Update codesecret.js

--- commands/codesecret.js
+++ commands/codesecret.js
@@ -3,10 +3,6 @@ const Discord = require('discord.js');
 exports.run = (client, message, args, user, command) => {
 
 
-const help = new Discord.RichEmbed()
-.setColor('RANDOM')
-.setTitle('CodeSecret')
-.setDescription(`La commande **${command.help.name}** est un genre de Easter Egg !\nEn effet, pour gagner il vous faudras trouver le juste prix !\n\n__Utilisation__ : .codesecret/.cs {nombre}`)
 
 
 if (args !== "14589724587854784587") return message.reply(`perdu !\n**${args.join(" ")}** n'est pas le bon nombre !`)
