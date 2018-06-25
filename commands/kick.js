ca3c06a6469da5a04b19d1d7a7566758c1901979
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-25 19:03:04 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
+  
+  if (message.member.hasPermission('KICK_MEMBERS') || message.member.hasPermission('ADMINISTRATOR') || message.author.id == 327176843709120512) {
+  
+  
+  
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('Je ne trouve pas de channel "mod-log". ');
+  let modlog = client.channels.find('name', 'izmod-log');
+  if (!modlog) return message.reply('Je ne trouve pas de channel "izmod-log". ');
   if (reason.length < 1) return message.reply('Vous devez mettre une raison pour kick !');
   if (message.mentions.users.size < 1) return message.reply("vous devez mentionner quelqu'un pour le kick.").catch(console.error);
 
