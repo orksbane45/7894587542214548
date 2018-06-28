64b9c83e8f7741663a5dc8009e8d8d6d11ce14bd
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-06-28 19:08:25 +0200

    Create serverInfo.js

---
+++ commands/serverInfo.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    
+    .setColor("RANDOM")
+    .setThumbnail(sicon)
+    .addField("Nom du serveur", message.guild.name)
+    .addField("Date de création du serveur", message.guild.createdAt)
+    .addField("Date d'arrivé", message.member.joinedAt)
+    .addField("Membres", message.guild.memberCount);
+
+    message.channel.send(serverembed);
+}
+
+ exports.conf = {
+        enabled: true,
+        guildOnly: false,
+        aliases: ["si"],
+        permLevel: 0
+      };
+
+      exports.help = {
+        name: 'serveurinfo',
+        description: 'Affiche les informations du serveurs !',
+        usage: 'serveurinfo'
+        aliase: ["si"]
+      }
