f8aadd37316a5d0e4954b48da16f94e18c962ce8
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-07-07 19:56:58 +0200

    Update serverInfo.js

--- commands/serverInfo.js
+++ commands/serverInfo.js
@@ -2,13 +2,24 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let sicon = message.guild.iconURL;
+    
+  let day = message.guild.createdAt.getDate()
+  let month = 1 + message.guild.createdAt.getMonth()
+  let year = message.guild.createdAt.getFullYear()
+  let hour = message.guild.createdAt.getHours()
+  let min = message.guild.createdAt.getMinutes()
+  let days = message.author.createdAt.getDate()
+  let months = 1 + message.author.createdAt.getMonth()
+  let years = message.author.createdAt.getFullYear()
+  let hours = message.author.createdAt.getHours()
+  let mins = message.author.createdAt.getMinutes()
+  
     let serverembed = new Discord.RichEmbed()
     
     .setColor("RANDOM")
     .setThumbnail(sicon)
     .addField("Nom du serveur", message.guild.name)
-    .addField("Date de création du serveur", message.guild.createdAt)
-    .addField("Date d'arrivé", message.member.joinedAt)
+    .addField("Date de création du serveur", `${day}`)
     .addField("Membres", message.guild.memberCount);
 
     message.channel.send(serverembed);
