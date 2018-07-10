51a653bfd53d7f40ead33db2cb5363f03acc7bd1
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-07-10 13:02:06 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
 const settings = require('../settings.json');
 exports.run = function(client, message, args) {
+  var distance = client.uptime;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  uptime = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
 
   const msg = new Discord.RichEmbed()
     .setColor('RANDOM')
@@ -9,7 +15,7 @@ exports.run = function(client, message, args) {
     .addField('Utilisateurs ',`${client.users.size}`, true)
     .addField('Serveurs ',`${client.guilds.size}`, true)
     .addField('Channels',`${client.channels.size}`, true)
-   
+    .addField('Uptime', uptime , true)
     .addField('Créateur ',`InsinityZ#0018`, true)
     .addField('Officiel server ','`.discord`')
     .addField('Stockage ',`18.1MB/500MB`)
