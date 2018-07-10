dd13293aa74fbac8700ce0753a3697e5bf045024
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-07-10 13:31:43 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -15,6 +15,7 @@ exports.run = function(client, message, args) {
     .addField('Utilisateurs ',`${client.users.size}`, true)
     .addField('Serveurs ',`${client.guilds.size}`, true)
     .addField('Channels',`${client.channels.size}`, true)
+    .addField('Status des membres', `**${message.guild.members.filter(o => o.presence.status === 'online').size}** En ligne\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** Inactif\n**${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size}** Ne pas déranger\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Déconnecté/Invisible\n**${message.guild.members.filter(s => s.presence.status === 'streaming').size}** Streaming`)
     .addField('Uptime', uptime , true)
     .addField('Créateur ',`InsinityZ#0018`, true)
     .addField('Officiel server ','`.discord`')
