f507c179478bd23a7856c0396ca06e2e0a8839e1
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-07-24 23:58:02 +0200

    Create slap.js

---
+++ commands/slap.js
@@ -0,0 +1,138 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let user = message.mentions.users.first();
+  let nb = Math.floor((Math.random() * 9) + 1);
+  if (message.mentions.users.size < 1) return message.reply("vous devez mentionner quelqu'un pour le hug.").catch(console.error);
+    if (nb == 1) {
+   msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/punch-slap-cat-anime-angry-gif-7930351')
+    message.channel.send(msg)
+  }
+  if (nb == 2) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/anime-asking-please-carry-me-like-aprincess-beg-ask-gif-9960249')
+    message.channel.send(msg)
+  }
+  if (nb == 3) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/fuuka-anime-cartoon-japanese-slap-gif-7527933')
+    message.channel.send(msg)
+  }
+  if (nb == 4) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/relationship-anime-fight-arguing-gif-9778017')
+    message.channel.send(msg)
+  }
+  if (nb == 5) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/konosuba-witch-anime-slap-angry-gif-8065158')
+    message.channel.send(msg)
+  }
+  if (nb == 6) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/slap-anime-gif-9955713')
+    message.channel.send(msg)
+  }
+  if (nb == 7) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/anime-slap-face-gif-10006370')
+    message.channel.send(msg)
+  }
+  if (nb == 8) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/anime-slap-girl-boy-gif-7919028')
+    message.channel.send(msg)
+  }
+  if (nb == 9) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/anime-slap-horse-slap-funny-gif-8562186')
+    message.channel.send(msg)
+  }
+  if (nb == 10) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/slap-blood-anime-gif-4961067')
+    message.channel.send(msg)
+  }
+  if (nb == 11) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/anime-slap-gif-5122897')
+    message.channel.send(msg)
+  }
+  if (nb == 12) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/hotd-slap-hit-anime-hitting-gif-5318916')
+    message.channel.send(msg)
+  }
+  if (nb == 13) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/animepound-slam-gif-4880762')
+    message.channel.send(msg)
+  }
+  if (nb == 14) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/oreimo-gif-10937039')
+    message.channel.send(msg)
+  }
+  if (nb == 15) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/slap-handaseishuu-narukotoishi-barakamonanime-barakamon-gif-5509136')
+    message.channel.send(msg)
+  }
+  if (nb == 16) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/oreimo-anime-slap-gif-10936993')
+    message.channel.send(msg)
+  }
+  if (nb == 17) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<:calin:442062827402821662> ${user.username}, vous avez reçu une baffe de ${message.author.username}`)
+    .setColor('RANDOM')
+    .setImage('https://tenor.com/view/no-angry-anime-slap-gif-7355956')
+    message.channel.send(msg)
+  }
+
+}
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+
+  exports.help = {
+    name: 'slap',
+    description: 'Baffe !',
+    usage: 'slap [mention]'
+  }
