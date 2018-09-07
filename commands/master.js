4169c91354f56e5cfa237a343a6f65cef0a72cac
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-09-07 21:59:08 +0200

    Update master.js

--- commands/master.js
+++ commands/master.js
@@ -4,6 +4,9 @@
   if (message.author.id === settings.ownerid) return message.reply("Tu est mon maître !")
   if (message.author.id === settings.g8go) return message.reply("Tu est mon second maître !")
   if (message.author.id === "353884181539061764") return message.reply("non tu est juste gay ! :joy: ")
+   if (message.author.id === "486922176503480320") return message.reply("Oh ! Mon dieux, tu est l'élu")
+    if (message.author.id === "312163272642265089") return message.reply(":cat: Azumie le pain de mie")
+    
        message.channel.send("Tu n'est pas mon maître")                       
 };
 
