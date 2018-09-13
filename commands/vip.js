98b01d5e40bc58ca26cf8b2e012b09200184de9e
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-09-13 17:11:44 +0200

    Create vip.js

---
+++ commands/vip.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const settings = require('../settings.json');
+exports.run = function(client, message, args) {
+
+let vipemebed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.addField("Tu veut être VIP ?","paypal.me/InsinityZ")
+.setDescription("prochaine MAJ")
+ 
+  
+  
+  
+ message.channel.send(vipembed);
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'vip',
+  description: 'vip sur le bot',
+  usage: 'vip'
+};
